3b177508a4816ac19d4f2db3106a21122866687e
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-03-15 16:43:17 +0530

    temp

--- API/Controllers/AsyncHelper.cs
+++ API/Controllers/AsyncHelper.cs
@@ -6,6 +6,8 @@ namespace API.Controllers
 {
     internal static class AsyncHelper
     {
+
+
         private static readonly TaskFactory TaskFactory = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
 
         public static void RunSync(Func<Task> func)
